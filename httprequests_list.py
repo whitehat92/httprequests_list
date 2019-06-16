4a9a92d572887cb5ae6be56976e9a33aeb8dc0d0
whitehat92 <ze_correia93@hotmail.com>
2019-06-16 17:09:30 +0100

    Update httprequests_list.py

--- httprequests_list.py
+++ httprequests_list.py
@@ -1,20 +1,13 @@
 import requests
 import sys
 
-# I need a host and then append the list from command line
-
-#list can only contain some word or an "/" after the word
-ler = open(sys.argv[1], 'r')
-#printavel = ler.read()
-
-
-
-for url in ler:
-    site = "<site>"
-    sendHTTPS = requests.get(site + str(url))
+headers = {'User-Agent': 'Mozilla/12.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+}
+list = open(sys.argv[1], 'r')
+for url in list:
+    target = open(sys.argv[0], 'r')
+    site = "<website"
+    sendHTTPS = requests.get(site + str(url), timeout=10, headers=headers)
     pedidoHTTPS = sendHTTPS.status_code
     cabecaHTTPS = sendHTTPS.headers
-    pedido = sendHTTPS.status_code
-    cabeca = sendHTTPS.headers
-    print (site + str(url) + "\n" + str(pedidoHTTPS))
-    print (cabecaHTTPS)
+    print (str(pedidoHTTPS) + "<------>" + site + str(url) + str(cabecaHTTPS))
